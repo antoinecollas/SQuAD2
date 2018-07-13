4f6aea0155af178afc742c9081c850fdd8b662bb
Antoine Collas <tamaiticollas@gmail.com>
2018-07-13 18:01:18 +0200

    test update

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from transformer import *
-input1 = torch.Tensor([[1,3,2,0,0],[1,52,2,0,0]])
+input1 = torch.Tensor([[10,3,2,0,0],[22,52,2,3,0]])
 input1 = input1.type(torch.LongTensor)
-tr = Transformer(100,5,5,nb_heads=2)
-print(tr.forward(input1))
+input2 = torch.Tensor([[1,15,2,0,0],[1,52,8,0,0]])
+input2 = input2.type(torch.LongTensor)
+tr = Transformer(100,100,5,5,nb_heads=2)
+print(tr.forward(input1, input2))
