bf82944179bc491bc75919b9858899d46c0995fe
Antoine Collas <tamaiticollas@gmail.com>
2018-07-13 18:01:11 +0200

    transformer

--- transformer.py
+++ transformer.py
@@ -22,35 +22,31 @@ class Transformer(nn.Module):
         self.Decoder = Decoder(n_layers, nb_heads, d_model, n_neurons, dropout)
         self.Linear = nn.Linear(d_model, vocabulary_size_out, bias=False)
 
-    def forward(self, X):
+    def forward(self, src, target):
         '''
         Arg:
-            X: tensor(nb_texts, nb_tokens)
+            src: tensor(nb_texts, nb_tokens)
+            target: tensor(nb_texts, nb_tokens)
         Output:
             tensor(nb_texts, nb_tokens, d_model(=size of one token))
             tensor(nb_texts, vocabulary_size)
             tensor(nb_texts, nb_tokens_out)
         '''
-        mask = get_mask(X,X)
-        enc = self.Embedding(X)
+        mask = get_mask(src,src)
+        enc = self.Embedding(src)
         enc = self.Encoder(enc,mask)
-        nb_texts = X.size(0)
-        phrases = torch.zeros(nb_texts, self.nb_tokens_out).type(torch.LongTensor)
+        mask1 = get_mask(target,target,avoid_subsequent_info=True)
+        mask2 = get_mask(target,src)
+        output = self.Embedding(target)
+        output = self.Decoder(enc, output, mask1, mask2)
+        output = self.Linear(output)
+        output = F.softmax(output, dim=-1)
+        values, output = torch.max(output, dim=-1)
+        nb_texts = src.size(0)
+        #we suppose that all target phrases have the same number of values!=0
+        j=0
+        while target[0][j]!=0:
+            j+=1
         for i in range(nb_texts):
-            phrases[i][0] = START_OF_SENTENCE_IDX
-        for i in range(1, self.nb_tokens_out):
-            print("itération n°", i)
-            print(phrases)
-            sequence_for_mask = torch.zeros(nb_texts, self.nb_tokens_out).type(torch.LongTensor)
-            sequence_for_mask[:,:i] = START_OF_SENTENCE_IDX
-            mask1 = get_mask(sequence_for_mask,sequence_for_mask,avoid_subsequent_info=True)
-            mask2 = get_mask(sequence_for_mask,X)
-            output = self.Embedding(phrases)
-            output = self.Decoder(enc, output, mask1, mask2)
-            output = self.Linear(output)
-            output = F.softmax(output, dim=-1)
-            values, output = torch.max(output, dim=-1)
-            for j in range(nb_texts):
-                phrases[j][i] = output[j][i]
-
-        return phrases
+            target[i][j] = output[i][j]
+        return target
