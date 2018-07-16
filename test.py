4af1caab4b7416716808f30261211152009b56b1
Antoine Collas <tamaiticollas@gmail.com>
2018-07-16 14:45:05 +0200

    seems to work with padding

--- test.py
+++ test.py
@@ -53,9 +53,11 @@ if False:
 
 if True:
     print("====TEST Translator====")
-    input1 = torch.Tensor([[10,3,2,1,4],[12,14,2,6,7]])
+    # input1 = torch.Tensor([[10,3,2,1,4],[12,14,2,6,7]])
+    input1 = torch.Tensor([[10,3,2,1,0],[12,14,2,6,0]])
     input1 = input1.type(torch.LongTensor)
-    input2 = torch.Tensor([[1,15,2,9,4],[1,12,8,11,6]])
+    # input2 = torch.Tensor([[1,15,2,9,4],[1,12,8,11,6]])
+    input2 = torch.Tensor([[1,15,19,0,0],[1,12,8,0,0]])
     input2 = input2.type(torch.LongTensor)
     # tr = Transformer(100,100,5,5,nb_heads=2)
     # print(tr(input1, input2))
