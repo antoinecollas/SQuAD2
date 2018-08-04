71cef17436673f5b1697e80f0f844ccda32d5636
Antoine Collas <tamaiticollas@gmail.com>
2018-08-04 14:15:36 +0200

    new dataset

--- train/constants.py
+++ train/constants.py
@@ -5,7 +5,7 @@ NCPUS=os.cpu_count()
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TYPE = torch.float32
 
-#constants for the preparation of WMT en_fr
+#constants for the preparation of dataset
 #/!\ the idx must be int in [0,3] and words must be different from vocabulary of the texts
 PADDING_IDX = 0
 UNKNOW_WORD_IDX = 1
@@ -21,13 +21,13 @@ MIN_FREQ = 0
 NUMP_OPS_BPE = 100
 
 #files
-FOLDER = "WMT_en_fr/"
+FOLDER = "europarl/"
 TRAIN_SUFFIX = ".train"
 TEST_SUFFIX = ".test"
 EN_SUFFIX = ".en"
 FR_SUFFIX = ".fr"
-SIZE = "100_000" #number of pair of phrases
-RAW = FOLDER + "giga-fren.release2.fixed_" + SIZE
+SIZE = "100" #number of pair of phrases
+RAW = FOLDER + "europarl_" + SIZE
 #bpe files
 CODES_FILE = FOLDER + "codes_file_" + SIZE + "_" + str(NUMP_OPS_BPE)
 VOCAB_FILE = FOLDER + "vocab_" + SIZE + "_" + str(NUMP_OPS_BPE)
@@ -44,16 +44,16 @@ TEST = PRETRAIN
 PATH_WEIGHTS = "./weights_" + SIZE
 #hyperparameters
 SHARE_WEIGHTS = True
-NB_LAYERS = 6
-NB_HEADS = 1
-D_MODEL = 512
-NB_NEURONS = 4096
-WARMUP_STEPS = 16000
+NB_LAYERS = 2
+NB_HEADS = 8
+D_MODEL = 64
+NB_NEURONS = 128
+WARMUP_STEPS = 100
 
 #constants for training
 TRAIN_SPLIT = 0.8
-NB_EPOCH = 10000
-BATCH_SIZE = 10
+NB_EPOCH = 1000
+BATCH_SIZE = 20
 MAX_SEQ = 256
 
-PREDICT_BATCH_SIZE = 100
+PREDICT_BATCH_SIZE = 10
