52ea4381a8eaea08b6d3e6278a90689ecbce4a3a
Antoine Collas <tamaiticollas@gmail.com>
2018-08-04 21:44:52 +0200

    constants

--- train/constants.py
+++ train/constants.py
@@ -18,7 +18,7 @@ UNKNOW_WORD = '_unk_'
 PADDING_WORD = '_pad_'
 MAX_VOCAB = 1000000
 MIN_FREQ = 0
-NUMP_OPS_BPE = 100
+NUMP_OPS_BPE = 30000
 
 #files
 FOLDER = "europarl/"
@@ -26,7 +26,7 @@ TRAIN_SUFFIX = ".train"
 TEST_SUFFIX = ".test"
 EN_SUFFIX = ".en"
 FR_SUFFIX = ".fr"
-SIZE = "100" #number of pair of phrases
+SIZE = "100_000" #number of pair of phrases
 RAW = FOLDER + "europarl_" + SIZE
 #bpe files
 CODES_FILE = FOLDER + "codes_file_" + SIZE + "_" + str(NUMP_OPS_BPE)
@@ -45,15 +45,15 @@ PATH_WEIGHTS = "./weights_" + SIZE
 #hyperparameters
 SHARE_WEIGHTS = True
 NB_LAYERS = 2
-NB_HEADS = 8
-D_MODEL = 64
-NB_NEURONS = 128
-WARMUP_STEPS = 100
+NB_HEADS = 4
+D_MODEL = 256
+NB_NEURONS = 1024
+WARMUP_STEPS = 16000
 
 #constants for training
 TRAIN_SPLIT = 0.8
 NB_EPOCH = 1000
-BATCH_SIZE = 20
-MAX_SEQ = 256
+BATCH_SIZE = 80
+MAX_SEQ = 100
 
-PREDICT_BATCH_SIZE = 10
+PREDICT_BATCH_SIZE = 80
